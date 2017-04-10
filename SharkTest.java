3095dcadd1f0d8c52dbd655e7356ee4784b48896
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-10 15:41:40 +0100

    before added to test file

--- SharkTest.java
+++ SharkTest.java
@@ -4,4 +4,10 @@ import org.junit.*;
 public class SharkTest{
   Shark shark;
   Person person;
+
+  @Before
+  public void before(){
+    shark = new Shark("Sheila");
+    person = new Person();
+  }
 }
